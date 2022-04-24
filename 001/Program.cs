92080d7210205b27de507f1f13073eb4695c6bb8
Lnosonova <lilipadnose@gmail.com>
2022-04-24 11:05:51 +0300

    C#.Seminar 4

---
+++ 001/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задать массив из 8 элементов и вывести их на экран
+// Задать массив из 8 элементов и вывести их на экран
+
+void FillArray (int[] Array)
+{
+    int length = Array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Array[index] = new Random().Next(0, 2);
+        index++;
+    } 
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+    {
+        System.Console.WriteLine(array[position]);
+        position++;
+        
+    }
+}
+int[] Array = new int[8];
+FillArray(Array);
+PrintArray(Array);
+
